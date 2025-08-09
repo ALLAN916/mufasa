cf8d56109b38f554da4324f8ca61620204a2686a
auto <auto@example.com>
2025-08-09 14:11:56 +0000

    perf: Tailwind globs, minify CSS, prod build, streamline index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./pages/*.{html,js}", "./index.html", "./js/*.js"],
+  content: ["./**/*.{html,js}", "!./node_modules/**/*", "!./dist/**/*"],
   theme: {
     extend: {
       colors: {
